1870f62e894cc47a34ae91ef550c0cb3bc0bae57
Akshaybaisware <130745994+Akshaybaisware@users.noreply.github.com>
2023-06-03 13:19:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function volume_sphere() {
     //Write your code here
-  event.preventDefault();
+  event.preventDefault(); // for form submit t
 	const radius = document.getElementById("radius").value;
 	const volume = (4 / 3 * Math.PI * radius ** 3);
-	document.getElementById("volume") = volume.toFixed(4);
+	document.getElementById("volume") = volume.toFixed(4); // for decimal 4hidigit tak
 } 
 
-window.onload =function()
+window.onload =function() // for time loading
 	{
 		document.getElementById('MyForm').addEventListener('submit' , volume_sphere);
 	}
