c2b782a689bcbd517220617bd62103aeb5421a6d
Akshaybaisware <130745994+Akshaybaisware@users.noreply.github.com>
2023-06-03 13:09:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function volume_sphere() {
     //Write your code here
-  
+  event.preventDefault();
+	const radius = document.getElementById("radius").value;
+	const volume = (4 / 3 * Math.PI * radius ** 3);
+	document.getElementById("volume") = volume.toFixed(4);
 } 
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+window.onload =function()
+	{
+		document.getElementById('MyForm').addEventlistener('submit' , volume_sphere);
+	}
